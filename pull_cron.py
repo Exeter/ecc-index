fb2ab8356952c5b2816a5b6ba8a5a3179e946bd4
Anthony Bau <dab1998@gmail.com>
2013-05-08 19:28:40 -0400

    Um. What?

--- pull_cron.py
+++ pull_cron.py
@@ -1,9 +1,14 @@
+#!/usr/bin/env python
+
 import os
 
 paths = os.listdir("/srv/http/cgi-bin")
+
+print (paths)
+
 os.chdir("/srv/http/cgi-bin")
 
-for name in path:
+for name in paths:
   os.chdir(name)
   os.system("git pull")
   os.chdir("/srv/http/cgi-bin")
